003aeec98ed9a93aa4609d73144e8171fb362d6e
Tomasz Tomczyk <me@tomasztomczyk.com>
2026-02-02 01:55:49 +0000

    Fix assets

--- lib/reposit_web.ex
+++ lib/reposit_web.ex
@@ -18,8 +18,7 @@ defmodule RepositWeb do
   """
 
   def static_paths,
-    do:
-      ~w(assets fonts images favicon.ico favicon.svg favicon-96x96.png apple-touch-icon.png web-app-manifest-192x192.png web-app-manifest-512x512.png site.webmanifest robots.txt llms.txt)
+    do: ~w(assets fonts images icons robots.txt llms.txt)
 
   def router do
     quote do
